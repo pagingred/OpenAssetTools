0cd486ceaa62cb796d7790f9f241d8d861df5959
njohnson <gitea.nicholasjohnson.info>
2026-07-15 13:19:42 -0400

    Add T5 MenuDef and MenuList ANTLR4 loaders.

---
+++ src/ObjLoading/Game/T5/Menu/RawLoaderMenuDefT5.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Asset/IAssetCreator.h"
+#include "Game/T5/T5.h"
+#include "SearchPath/ISearchPath.h"
+#include "Utils/MemoryManager.h"
+
+#include <memory>
+
+namespace menu
+{
+    std::unique_ptr<AssetCreator<T5::AssetMenu>> CreateRawLoaderMenuDefT5(MemoryManager& memory, ISearchPath& searchPath);
+} // namespace menu
